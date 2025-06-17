d8a7d0be0f0372c2d59ed0c5762773ebabac4f35
Luana <luanamr2222@gmail.com>
2025-06-17 12:45:36 -0300

    fix bugs: error build

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 
 import PokemonProvider from "./context/usePokemon";
 import Router from "./routes";
-import "./styles/global.module.scss";
+import "./styles/global.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <PokemonProvider>
